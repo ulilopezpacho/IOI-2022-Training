6d36b4b8bde3c392e55260be06ee589592eac5a0
Ulises Lopez Pacholczak <54071158+ulilopezpacho@users.noreply.github.com>
2022-06-22 18:36:25 +0000

    Mejorando ada and spieces

---
+++ Divide and Conquer/AdaAndSpiecesTesting/AdaAndSpiecesCuadratico.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define forn(i,n) for(int i = 0; i < n; i++)
+
+using namespace std;
+const int rows = 4;
+
+#define DBG(x) cerr << #x << " = " << (x) << endl 
+#define DBG2(x,y) cerr << #x << " = " << (x) << " , " << #y << " = " << (y) << endl 
+#define SIZE(c) int((c).size())
+
+template <typename T>
+ostream & operator <<(ostream &os, const vector<T>& v) {
+    os << "[";
+    forn(i,SIZE(v))
+    {
+        if (i > 0) os << " ";
+        os << v[i];
+    }
+    return os << "]";
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<vector<int>> nums (n, vector<int> (rows));
+    vector<bool> tachados(n, false);
+
+    forn (i,n) forn (j, rows) cin >> nums[i][j];
+    
+    forn (i,n) {
+        forn (j,n) {
+            bool esPisado = true;
+            forn (r, rows) {
+                if (nums[i][r] <= nums[j][r]) esPisado = false;
+            }
+            tachados[i] = tachados[i] | esPisado;
+        }
+    }
+
+    int ans = 0;
+    for (auto e: tachados) {
+        if (!e) ans++;
+    }
+
+    cout << ans << "\n";
+}
---
+++ Divide and Conquer/AdaAndSpiecesTesting/AdaAndSpiecesGenerador.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define forn(i,n) for(int i = 0; i < n; i++)
+
+using namespace std;
+
+int main () {
+    srand(time(0));
+    vector<set<int>> salieron (4);
+    int n = 8;
+
+}
